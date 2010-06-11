af1795fc76869aed6703bf10386e0501d8d29e9a
Alex Averbuch <alex.averbuch@gmail.com>
2010-06-11 19:38:43 +0200

    + added MakeGML application
    Signed-off-by: Alex Averbuch <alex.averbuch@gmail.com>

--- src/main/java/applications/MakeGML.java
+++ src/main/java/applications/MakeGML.java
@@ -6,7 +6,7 @@ import graph_gen_utils.memory_graph.MemGraph;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
 
-public class MakeMetrics {
+public class MakeGML {
   
   /**
    * @param args
@@ -14,17 +14,16 @@ public class MakeMetrics {
   public static void main(String[] args) {
     
     if (args[0].equals("help")) {
-      System.out.println("Params - " + "MetricsFilePath:Str "
+      System.out.println("Params - " + "GMLFilePath:Str "
         + "Neo4jDirectory:Str");
       return;
     }
     
-    String metricsPath = args[0];
+    String gmlPath = args[0];
     String dbDir = args[1];
     
     GraphDatabaseService db = new EmbeddedGraphDatabase(dbDir);
-    MemGraph memDb = NeoFromFile.readMemGraph(db);
-    NeoFromFile.writeMetricsCSV(memDb, metricsPath);
+    NeoFromFile.writeGMLBasic(db, gmlPath);
     
     db.shutdown();
     
